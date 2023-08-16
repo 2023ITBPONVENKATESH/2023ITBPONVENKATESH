2e7bc2b027ae22a59e2c95bc385dfa63bedd35ee
2023ITBPONVENKATESH <139753291+2023ITBPONVENKATESH@users.noreply.github.com>
2023-08-16 15:40:01 +0530

    Java . hello world

---
+++ README.m
@@ -0,0 +1,16 @@
+pabulic class hello world 
+{
+  pabulic  static viod main (String args[])
+  {
+    system.ouy.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+    system.out.println("hello,world");
+  }
+}
